d74be4a0059501a001b8352a97f041362e0e096c
HeiBa <50979290+2445868686@users.noreply.github.com>
2025-05-27 18:37:14 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 # -*- coding: utf-8 -*-
 """
 main.py
-主程序入口，只负责初始化和启动 Tkinter 应用。
+主程序入口，只负责初始化和启动 PyQt5 应用。
 """
-from ui import App
+import sys
+from PyQt5.QtWidgets import QApplication
+from ui import MainWindow  # 从 ui.py 导入 MainWindow
 
 def main():
-    app = App()
-    app.mainloop()
+    app = QApplication(sys.argv)
+    app.setStyle("Fusion")  # 设置一个现代的UI风格
+
+    main_window = MainWindow()
+    main_window.show()  # 允许窗口默认可手动调整大小
+
+    sys.exit(app.exec_())
 
 if __name__ == "__main__":
     main()
